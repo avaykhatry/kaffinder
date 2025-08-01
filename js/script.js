6cbcaa6d8353af6f44d6a3655f79a7e971ec4efa
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-08-01 21:46:38 +0545

    Add localStorage to persist last active filter tag

--- js/script.js
+++ js/script.js
@@ -1,94 +1,87 @@
 document.addEventListener('DOMContentLoaded', function() {
-const cafeList = document.getElementById("cafeList");
-
-// function renderCafes(data) {
-//   cafeList.innerHTML = "";
-
-//   data.forEach(cafe => {
-//     const card = document.createElement("div");
-//     card.className = "card";
-//     card.innerHTML = `
-//       <img src="${cafe.imageUrl}" alt="${cafe.name}">
-//       <div class="card-content">
-//         <h3>${cafe.name}</h3>
-//         <p>${cafe.description}</p>
-//         <p>☕ ₹${cafe.price}</p>
-//         <div class="card-tags">${cafe.tags.join(", ")}</div>
-//       </div>
-//     `;
-//     cafeList.appendChild(card);
-//   });
-// }
-
-// to fetch cafe data
+  // to fetch cafe data
   let cafeFile = '../cafes.json';
+    // fetch(cafeFile)
+    // .then(response => response.json())
+    // .then(cafeData => {
+    //   renderCafes(cafeData);
+    // });
+    
+
+  const cafeList = document.getElementById("cafeList");
+  const tagButtons = document.querySelectorAll("#tagFilters button");
+
+
+// let currentTag = 'all';
+if (!localStorage.getItem('currentTag')) {
+  localStorage.setItem('currentTag', 'all');
+}
+
+filterCafes();
+
+//function for cafe data rendering
+function renderCafes(data) {
+
+  cafeList.innerHTML = "";
+  for (let i = 0; i < data.length; i++ ) {
+      const cafeName = data[i].name;
+      const cafeLocation = data[i].location;
+
+      const li = document.createElement('li');
+      li.className = "card";
+      li.innerHTML = `
+      <img src='${data[i].photo}'>
+        <div class='card-content'>
+          <h3>${cafeName}</h3>
+          <p>${cafeLocation}</p>
+          <div class='card-tags'>${data[i].tags.join(", ")}</div>
+        </div>
+      `;
+
+      cafeList.append(li);
+    }
+}
+
+//function for filtering tag here
+function filterCafes() {
+  
   fetch(cafeFile)
   .then(response => response.json())
   .then(cafeData => {
-    for (let i = 0; i < cafeData.length; i++ ) {
-      const allCafe = cafeData[i].name;
-      console.log(allCafe);
-    };
+    let currentTag = localStorage.getItem('currentTag');
+    let filtered = cafeData;
+
+    if (currentTag !== 'all') {
+      filtered = filtered.filter(cd => cd.tags.includes(currentTag));
+    }
+    localStorage.setItem('currentTag', currentTag);
+
+    renderCafes(filtered);
+
+    const activeTag = Array.from(tagButtons).find(btn => btn.dataset.tag === currentTag);
+
+    if (activeTag) {
+    activeTag.classList.add('active');
+    }
+
+    console.log(filtered);
+  })
+  };
+
+
+
+
+
+  
+  tagButtons.forEach(function(button) {
+    button.onclick = function() {
+      tagButtons.forEach(button => button.classList.remove("active"));
+      button.classList.add("active");
+      currentTag = button.dataset.tag;
+      localStorage.setItem('currentTag', currentTag);
+      filterCafes();
+      
+    }
   });
 
 });
-  
-
-
-// const cafes = [
-//   {
-//     name: "Café Doppio",
-//     price: 140,
-//     tags: ["quiet", "free-wifi", "laptop-friendly"],
-//     imageUrl: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
-//     description: "Minimal, peaceful, great for deep work"
-//   },
-//   {
-//     name: "Brew Bros",
-//     price: 200,
-//     tags: ["outdoor-seating", "free-wifi"],
-//     imageUrl: "https://images.unsplash.com/photo-1509042239860-f550ce710b93",
-//     description: "Chill vibes, good for group study"
-//   },
-//   {
-//     name: "Nomad Café",
-//     price: 110,
-//     tags: ["quiet", "laptop-friendly"],
-//     imageUrl: "https://images.unsplash.com/photo-1555396273-367ea4eb4db5",
-//     description: "Budget gem with great seating"
-//   }
-// ];
-
-// const priceFilter = document.getElementById("priceFilter");
-// const tagButtons = document.querySelectorAll("#tagFilters button");
-
-// let currentTag = "all";
-
-
-// function filterCafes() {
-//   const price = priceFilter.value;
-//   let filtered = cafes;
-// zx
-//   if (price !== "all") {
-//     filtered = filtered.filter(c => c.price <= parseInt(price));
-//   }
-
-//   if (currentTag !== "all") {
-//     filtered = filtered.filter(c => c.tags.includes(currentTag));
-//   }
-
-//   renderCafes(filtered);
-// }
-
-// priceFilter.addEventListener("change", filterCafes);
-
-// tagButtons.forEach(btn => {
-//   btn.addEventListener("click", () => {
-//     tagButtons.forEach(b => b.classList.remove("active"));
-//     btn.classList.add("active");
-//     currentTag = btn.dataset.tag;
-//     filterCafes();
-//   });
-// });
-
-// renderCafes(cafes);
